51fc5be884b69b2395f7971d0e40f9ce92eeacf2
tkkxttys <kittycatty.2548@gmail.com>
2025-03-13 18:17:40 +0700

    initial commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import InteractiveCard from './InteractiveCard'
+
+export default function ProductCard ( {venueName, imgSrc} :  {venueName: string, imgSrc: string} ) {
+
+    return (
+        <InteractiveCard contentName={venueName}>
+            <div className='w-full h-[70%] relative rounded-t-lg'>
+                <Image src={imgSrc}
+                alt='Product Picture'
+                fill={true}
+                objectFit='cover'
+                className='object-cover rounded-t-lg'/>
+            </div>
+            <div className='w-full h-[30%] p-[10px]'>{venueName}</div>
+        </InteractiveCard>
+    )
+}
